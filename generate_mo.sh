d7e3182b9ebde4cfefa630c13b014c345be41563
Christopher Schwardt <christopher.schwardt@gmx.de>
2022-06-05 22:13:52 +0200

    add slovenian translation #21 (thanks MatejKovacic)

--- generate_mo.sh
+++ generate_mo.sh
@@ -5,3 +5,10 @@
 
 mkdir -p mo/de_DE.UTF-8/LC_MESSAGES/
 msgfmt i18n/de.po -o mo/de_DE.UTF-8/LC_MESSAGES/usbguard_gnome.mo
+
+mkdir -p mo/en_US.UTF-8/LC_MESSAGES/
+msgfmt i18n/en.po -o mo/en_US.UTF-8/LC_MESSAGES/usbguard_gnome.mo
+
+mkdir -p mo/sl_SI.UTF-8/LC_MESSAGES/
+msgfmt i18n/sl.po -o mo/sl_SI.UTF-8/LC_MESSAGES/usbguard_gnome.mo
+
